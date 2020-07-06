8827add2f3ad00686042568ba344f96a912a9f5f
locus <spec_oops@naver.com>
2020-07-06 22:17:20 +0900

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use async_std::{
+    prelude::*,
+    task,
+    io::BufReader,
+    net::TcpStream,
+    net::{TcpListener, ToSocketAddrs},
+};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
+    let listener = TcpListener::bind(addr).await?;
+    let mut incoming = listener.incoming();
+    while let Some(stream) = incoming.next().await {
+        let stream = stream?;
+        println!("Accepting from: {}", stream.peer_addr()?);
+        let _handle = spawn_and_log_error(connection_loop(stream));
+    }
+    Ok(())
+}
+
+async fn connection_loop(stream: TcpStream) -> Result<()> 
+{
+    let reader = BufReader::new(&stream);
+    let mut lines = reader.lines();
+
+    let name = match lines.next().await {
+        None => Err("peer disconnected immediatly")?,
+        Some(line) => line?,
+    };
+    println!("name = {}", name);
+
+    while let Some(line) = lines.next().await {
+        let line = line?;
+        let (dest, msg) = match line.find(':') {
+            None => continue,
+            Some(idx) => (&line[..idx], line[idx + 1 ..].trim()),
+        };
+        let dest: Vec<String> = dest.split(',').map(|name| name.trim().to_string()).collect();
+        let msg: String = msg.to_string();
+    }
+    Ok(())
+}
+
+fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
+where F: Future<Output = Result<()>> + Send + 'static,
+{
+    task::spawn(async move {
+        if let Err(e) = fut.await {
+            eprintln!("{}", e);
+        }
+    })
+}
+
+//main
+fn main() -> Result<()> {
+    let fut = accept_loop("127.0.0.1:8080");
+    task::block_on(fut)
+}
